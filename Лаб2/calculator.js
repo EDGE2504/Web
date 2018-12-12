8beddf6597a43cd87942f42d0099fd34e7185604
EDGE2504 <39498169+EDGE2504@users.noreply.github.com>
2018-12-12 19:41:21 +0200

    Lab 2

---
+++ Лаб2/calculator.js
@@ -0,0 +1,113 @@
+//Global variables
+var results = document.getElementById("display");
+var inputs = ["","",""];
+var values = [];
+var funOn = 0;
+var dotOn = 0;
+// Arithmetic functions
+var add = function(a, b) {return a + b;}
+var subtract = function(a, b) {return a - b;}
+var multiply = function(a, b) {return a * b;}
+var divide = function(a, b) {return a / b;}
+var fsqrt = function(a) {return Math.sqrt(a);}
+var degree = function(a, b) {return Math.pow(a,b);} 
+var equals = function() {
+	if (inputs[1] === "+") {
+		var sum = add(parseFloat(inputs[0]), parseFloat(inputs[2]));
+		clear();	
+		values.push(sum);
+	}
+	else if (inputs[1] === "-") {
+		var difference = subtract(parseFloat(inputs[0]), parseFloat(inputs[2]));
+		clear();	
+		values.push(difference)
+	}
+	else if (inputs[1] === "*") {
+		var product = multiply(parseFloat(inputs[0]), parseFloat(inputs[2]));
+		clear();	
+		values.push(product);	
+	}
+	else if (inputs[1] === "/") {
+		var quotient = divide(parseFloat(inputs[0]), parseFloat(inputs[2]));
+		clear();
+		values.push(quotient);
+	}
+	else if (inputs[1] ==="^") {
+		var degrees = degree(parseFloat(inputs[0]), parseFloat(inputs[2]));
+		clear();
+		values.push(degrees);
+	}
+	display();
+}
+
+// Functions for storing and displaying user input
+var update = function(value) {
+	inputs.push(value);
+	inputs.shift();
+}
+var clear = function() {
+	inputs = ["","",""];
+	values = [];
+	display();
+}
+var display = function() {
+	results.innerHTML = inputs.join(" ") + " " + values.join("");
+}
+
+// Event listeners for mouse input
+for (var i = 0; i < 10; i++) {
+	document.getElementById(i).addEventListener("click", function() {
+		if(values.length < 18){
+		values.push(this.innerHTML);
+		display();
+		}
+	});
+}
+document.getElementById(10).addEventListener("click", function() {
+	if(dotOn == 0) 
+		{
+		values.push(this.innerHTML);
+		display();
+		dotOn = 1;
+		}
+	});
+for (var i = 11; i < 16; i++) {
+	document.getElementById(i).addEventListener("click", function() {
+		if(funOn == 1) 
+		{
+			update(values.join(""));
+			values = [];
+			equals();
+		}
+		update(values.join(""));
+		update(this.innerHTML);
+		values = [];
+		funOn = 1;
+		dotOn = 0;
+		display();
+	});
+}
+document.getElementById(18).addEventListener("click", function(){
+	if(funOn == 1) 
+		{
+			update(values.join(""));
+			values = [];
+			equals();
+			funOn = 0;
+		}
+	update(values.join(""));
+	values = [];
+	var ssqrt = fsqrt(parseFloat(inputs[2]));
+	clear();
+	values.push(ssqrt);
+	display();
+});
+document.getElementById(16).addEventListener("click", function(){
+	update(values.join(""));
+	values = [];
+	equals();
+	funOn = 0;
+	dotOn = 0;
+});
+document.getElementById(17).addEventListener("click", function(){ clear(); });
+
